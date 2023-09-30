8f9c7daa886b9b6d22336ae70175925e7fef5c04
Евгений Петухов <petukhov.evgenyy@gmail.com>
2023-09-30 13:58:33 +0300

    Добавьте файлы проекта.

---
+++ RBM/Source.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() {
+    setlocale(0, "");
+    cout << "Размер int: " << sizeof(int) << " байта" << endl;
+    cout << "Размер short int: " << sizeof(short int) << " байта" << endl;
+    cout << "Размер long int: " << sizeof(long int) << " байта" << endl;
+    cout << "Размер float: " << sizeof(float) << " байта" << endl;
+    cout << "Размер double: " << sizeof(double) << " байт" << endl;
+    cout << "Размер long double: " << sizeof(long double) << " байт" << endl;
+    cout << "Размер char: " << sizeof(char) << " байт" << endl;
+    cout << "Размер bool: " << sizeof(bool) << " байт" << endl;
+    
+    return 0;
+}
